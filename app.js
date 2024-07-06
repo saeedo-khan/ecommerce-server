4e0b47c326958104e396068eb41f171bdd0b8786
saeedo-khan <smk1995m@gmail.com>
2024-07-06 17:57:05 +0300

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const productRoutes = require("./src/routes/v1/productRoutes")
 
 
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "50mb " }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // app.use(express.static(path.join(__dirname, "public")))
